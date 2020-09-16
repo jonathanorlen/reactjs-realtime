45565fc0bc755ac38b2f17503352cfbeec34c89d
jonathanorlen <pellondou.jo@gmail.com>
2020-09-16 20:01:53 +0700

    lazy query gql

--- src/App.js
+++ src/App.js
@@ -1,43 +1,20 @@
 import React, {useState} from 'react';
 import ApolloClient from 'apollo-boost';
 import {gql} from 'apollo-boost';
+import {ApolloProvider} from '@apollo/react-hooks';
+
+import Home from './pages/Home';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
 });
 
 const App = () => {
-  const [posts, setPosts] = useState([])
-  client.query({
-    query: gql` 
-      {allPosts{
-        id
-        title
-        description
-      }}
-    `
-  })
-  .then(result => setPosts(result.data.allPosts))
-
+  
   return (
-    <div className="container p-5">
-      <div className="row p-5">
-        {posts.map((post) => (
-          <div className="col-md-4" key={post.id}>
-            <div className="card">
-              <div className="card-body">
-              <div className="card-title">
-              {post.title}
-              </div>
-              <div className="card-text">
-                {post.description}
-              </div>
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <ApolloProvider client={client}>
+      <Home/>
+   </ApolloProvider>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
+import {useQuery, useLazyQuery} from '@apollo/react-hooks';
+
+const GET_ALL_POST = gql`
+     {
+          allPosts{
+               id
+               title
+               description
+          }
+     }
+`
+
+const client = new ApolloClient({
+     uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+});
+
+const Home = () => {
+     const { data, loading, error } = useQuery(GET_ALL_POST);
+     const [fetchPost, { data: posts}] = useLazyQuery(GET_ALL_POST);
+
+     if (loading) return <p className="p-5">Loading...</p>
+     return (
+          <div className="container p-5">
+               <div className="row p-5">
+                    {data.allPosts.map((post) => (
+                         <div className="col-md-4" key={post.id}>
+                              <div className="card">
+                                   <div className="card-body">
+                                        <div className="card-title">
+                                             {post.title}
+                                        </div>
+                                        <div className="card-text">
+                                             {post.description}
+                                        </div>
+                                   </div>
+                              </div>
+                         </div>
+                    ))}
+               </div>
+               <div className="row p-5">
+                    <button onClick={() => fetchPost()} className="btn btn-raised btn-primary"> Fetch Post</button>
+               </div>
+               <hr/>
+               {JSON.stringify(posts)}
+          </div>
+     );
+}
+
+export default Home;
