01756bae0f01909278d9c8fbf4c6988f583be1fc
jonathanorlen <pellondou.jo@gmail.com>
2020-09-17 00:05:38 +0700

    user register form

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Login = () => {
+     const [email, setEmail] = useState('');
+     const [loading, setLoading] = useState('');
+
+     const handleSubmit = () => {
+
+     }
+
      return (
      <div className="container p-5">
           <div className="row p-5">
                <h4>Login</h4>
+               <form onSUbmit={handleSubmit} >
+                    <div className="form-group">
+                         <label>Email Addres</label>
+                              <input 
+                                   type="email" 
+                                   className="form-control" 
+                                   value={email} 
+                                   onChange={(e) => setEmail(e.target.value)} 
+                                   placeholder="Enter Email"
+                                   disabled={loading}
+                              />
+                    </div>
+               </form>
           </div>
      </div>
      )
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+     const [email, setEmail] = useState('');
+     const [loading, setLoading] = useState('');
+
+     const handleSubmit = () => {
+
+     }
+
      return (
      <div className="container p-5">
           <div className="row p-5">
+               <div className="col-md-12">
                <h4>Register</h4>
+                    <form onSUbmit={handleSubmit} >
+                         <div className="form-group">
+                              <label>Email Address</label>
+                              <input
+                                   type="email"
+                                   className="form-control"
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)}
+                                   placeholder="Enter Email"
+                                   disabled={loading}
+                              />
+                         </div>
+                         <button className="btn btn-raised btn-primary" disabled={!email || loading}>
+                              Submit
+                         </button>
+                    </form>
+               </div>
           </div>
      </div>
      )
